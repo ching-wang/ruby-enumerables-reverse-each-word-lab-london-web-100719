66870aa1b45cc9c810f8df40de07bf9c2facb67b
Qing Wang <chingwang.qw@gmail.com>
2019-09-21 20:24:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(sentence) 
-	sentence.split(" ")
-	sentence.each do {|str|  str }
-end 
+def reverse_each_word(sentence1) 
+  stentence1.chars.sort_by.with_index { |_, i| -i }.join
+end
+
 

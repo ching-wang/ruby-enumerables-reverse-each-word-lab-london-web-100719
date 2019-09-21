b67981bcbaf2c7c42c52c3c4fbee4ce890a47db1
Qing Wang <chingwang.qw@gmail.com>
2019-09-21 20:31:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,4 @@
 def reverse_each_word(sentence1) 
 	my_array = sentence1.split(" ")
 	my_array.each do {|str| my_array.unshift}.join('') }
-	
 end 
-
-def reverse_chars
-    each_char.inject(""){|str, char| str.insert(0, char) }
-  end
